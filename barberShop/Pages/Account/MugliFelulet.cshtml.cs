5204a1868666f0e2e1a81fc4c5767608258ba037
OfficialCritical <szaszakpeterofficial@gmail.com>
2026-03-13 12:25:50 +0100

    priv

---
+++ barberShop/Pages/Account/MugliFelulet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace barberShop.Pages.Account
+{
+    public class MugliFeluletModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
