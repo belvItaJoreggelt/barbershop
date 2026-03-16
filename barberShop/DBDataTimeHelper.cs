ebd6cab4a1dd80b52d3cf193bbc5bdb457ea8f87
OfficialCritical <szaszakpeterofficial@gmail.com>
2026-03-16 14:24:37 +0100

    dbase

---
+++ barberShop/DBDataTimeHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace barberShop
+{
+    public static class DBDataTimeHelper
+    {
+        public static DateTime ToUtcDate(DateTime d)
+            => DateTime.SpecifyKind(d.Date,DateTimeKind.Utc);
+
+        public static DateTime ToUtc(DateTime local)
+            => DateTime.SpecifyKind(local, DateTimeKind.Local).ToUniversalTime();
+    }
+}
