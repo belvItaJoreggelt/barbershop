fcc3fa980c5bc7f5624e861db45805bb7d1dff10
OfficialCritical <szaszakpeterofficial@gmail.com>
2026-03-16 15:51:09 +0100

    dbase

---
+++ barberShop/Pages/Kapcsolat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace barberShop.Pages
+{
+    public class KapcsolatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
