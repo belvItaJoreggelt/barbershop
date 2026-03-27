42cc9c696cea4ead106237e59e9d21a697c1886c
OfficialCritical <szaszakpeterofficial@gmail.com>
2026-03-27 16:42:38 +0100

    s

--- barberShop/Pages/Privacy.cshtml.cs
+++ barberShop/Pages/Privacy.cshtml.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 
 namespace barberShop.Pages
 {
     public class PrivacyModel : PageModel
     {
+
+        [BindProperty(SupportsGet =true)]
+        public string Section { get; set; } = "";
+
+        [BindProperty(SupportsGet=true)]
+        public int? FodraszId { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public int? SzolgaltatasId { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string? FoglalasDatum { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string? FoglalasIdo { get; set; }
+
+
+
+
+
+
+
         public void OnGet()
         {
         }
