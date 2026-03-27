fca4c1c515b25d75feba3e15dba19c62c7f0e6ec
OfficialCritical <szaszakpeterofficial@gmail.com>
2026-03-27 14:48:54 +0100

    s

---
+++ barberShop/Pages/TorlesiSzab.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace barberShop.Pages
+{
+    public class TorlesiSzabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
